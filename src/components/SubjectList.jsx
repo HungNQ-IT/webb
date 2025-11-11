4a795ff2bfc744e54665571254217255441e4a28
hung <hungquocnguyen252@gmail.com>
2025-11-11 15:42:52 +0700

    ll

--- src/components/SubjectList.jsx
+++ src/components/SubjectList.jsx
@@ -63,7 +63,7 @@ function SubjectList({ quizzes }) {
     </div>
   )
 }
-//them vao check
+//themcheck
 return (
 <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 py-12">
       <div className="container mx-auto px-4">
